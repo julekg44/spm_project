391c271c9283c5c63f94fd36162b2f948e0476c6
Giuliano Galloppi <34445700+julekg44@users.noreply.github.com>
2022-08-22 13:48:17 +0200

    Update ripetizione.cpp

--- ripetizione.cpp
+++ ripetizione.cpp
@@ -16,7 +16,7 @@ printf("Hello world :) \n");
 //----------------------------------------------------------
 
 
-// aggiunto a GIT il 22/08/22
+// aggiunto a GIT il 22/08/22 ok
 // Created by Giuliano Galloppi on 26/03/22.
 //
 //  RIPETIZIONE =
