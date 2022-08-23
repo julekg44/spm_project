e307bc0c1434e2ba9b5be958a3d76dea3d4081de
Giuliano Galloppi <34445700+julekg44@users.noreply.github.com>
2022-08-23 14:11:04 +0000

    commit - opened clion

--- cmake-build-debug/CMakeFiles/progetto.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/progetto.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/tmp/progetto/main_seq.cpp" "CMakeFiles/progetto.dir/main_seq.cpp.o" "gcc" "CMakeFiles/progetto.dir/main_seq.cpp.o.d"
+  "/tmp/spm_project/main_seq.cpp" "CMakeFiles/progetto.dir/main_seq.cpp.o" "gcc" "CMakeFiles/progetto.dir/main_seq.cpp.o.d"
   )
 
 # Targets to which this target links.
