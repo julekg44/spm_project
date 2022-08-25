f1c1a8e5f7ade845e92f0f9d0b6a9478642713e9
Giuliano Galloppi <34445700+julekg44@users.noreply.github.com>
2022-08-25 15:33:09 +0000

    inizio sequenziale

--- main_seq.cpp
+++ main_seq.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "util.hpp"
 
 //std::cout == using namespace; cout<<
@@ -7,8 +8,85 @@ using std::cout; // program uses cout
 using std::cin; // program uses cin
 using std::endl; // program uses endl”
 */
+
+using namespace std;
+
+void printMatrix(int* A[],int n);
+void initTest(int n_input,int* n_len, int* A[], int x[], int B[]); //* QUI:  FIRMA/PROT: f(&a)     -> MAIN: f(a); con gli array vale lo stesso
+
+
+
+
+
+
+
 int main() {
 
+    //A x = B
+    //A[][] matrice
+    //x vettore di variabili di lunghezza 'n'
+    //n lunghezza di 'x'
+    //B vettore di termini conosciuti di lunghezza n
+    printStr("===== Inizio Jacobi Sequenziale =====\n");
+    int n_len_vector = 0; //lunghezza n del vettore    
+    int** matriceA; //Matrice
+    int* vettoreX; //vettore x di lunghezza n
+    int* vettoreB; // vettore risultato di lunghezza n
+
+    //printStr("Inserisci n elementi matrice");
+    //cin>>n_len_vector;
+
+    initTest(2,&n_len_vector,matriceA,vettoreX,vettoreB);
+    cout<<"il valore cambiato di n_len è:"<<n_len_vector<<endl;
+    printMatrix(matriceA,n_len_vector);
+
+
+
+
+
+
+    //tutto il calcolo del for mi serve a trovare le x
+
+
+
+
+    cout<<"Fine programma"<<endl;
+    return 0;
+}
+
+
+
+
+
+//FUNCTIONS----
+void initTest(int n_input,int* n_len, int* A[], int x[], int B[]){
+    *n_len=n_input;
+    for(int i =0;i<*n_len;i++){
+        A[i] = new int[*n_len];//allocazione dinamica
+        for(int j=0;j<*n_len;j++){
+            A[i][j] = j;
+        }
+    }
+}
+
+
+void printMatrix(int* A[],int n){
+    for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("Elem %d,%d is = %d \n",i,j,A[i][j]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
     std::cout << "Hello world xd" << std::endl;
     printTest();
     printStr("Prima\n");
@@ -25,10 +103,4 @@ int main() {
 
     std::cout << "x=" << x << std::endl;
     std::cout << "y=" << y << std::endl;
-
-
-
-
-    //endl == \n;
-}
-
+*/
