d76541e0a00a3305d69cea5fdee5a0d6822d5ab9
Giuliano Galloppi <34445700+julekg44@users.noreply.github.com>
2022-08-29 17:35:33 +0200

    commit

--- 1.concurrency/recap_thread.cpp
+++ 1.concurrency/recap_thread.cpp
@@ -1,3 +1,24 @@
 //
 // Created by Giuliano Galloppi on 29/08/22.
 //
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+#include <thread>
+
+using namespace std;
+void hello();
+
+int main(){
+
+    //ogni thread ha bisogno di una funzione iniziale/in ingresso
+    thread t(hello);
+
+    return 0;
+}
+
+void hello()
+{
+    std::cout<<"Hello Concurrent World\n";
+}
